09d67b7d3a7a04ffb3dd0f89ae43486a48d5111e
Dinis Cruz <dinis.cruz@owasp.org>
2025-08-03 01:00:50 +0100

    fixed main deploy script

--- tests/deploy_aws/test_Deploy__Service__to__main.py
+++ tests/deploy_aws/test_Deploy__Service__to__main.py
@@ -10,28 +10,28 @@ from mgraph_ai_service_github_digest.utils.deploy.Deploy__Service       import D
 class test_Deploy__Service__to__main(TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.deploy_fast_api__dev  = Deploy__Service(stage = 'main')
+        cls.deploy_fast_api__main  = Deploy__Service(stage = 'main')
 
-        with cls.deploy_fast_api__dev as _:
+        with cls.deploy_fast_api__main as _:
             if _.aws_config.aws_configured() is False:
                 pytest.skip("this test needs valid AWS credentials")
 
     def test_1__check_stages(self):
-        assert self.deploy_fast_api__dev .stage == 'dev'
+        assert self.deploy_fast_api__main .stage == 'main'
 
     def test_2__upload_dependencies(self):
-        upload_results = self.deploy_fast_api__dev.upload_lambda_dependencies_to_s3()
+        upload_results = self.deploy_fast_api__main.upload_lambda_dependencies_to_s3()
         assert list_set(upload_results) == LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS
 
     def test_3__create(self):
-        assert self.deploy_fast_api__dev .create() is True
+        assert self.deploy_fast_api__main .create() is True
 
     def test_4__invoke(self):
-        assert self.deploy_fast_api__dev .invoke().get('errorMessage') == DEFAULT__ERROR_MESSAGE__WHEN_FAST_API_IS_OK
+        assert self.deploy_fast_api__main .invoke().get('errorMessage') == DEFAULT__ERROR_MESSAGE__WHEN_FAST_API_IS_OK
 
     def test_4__invoke__function_url(self):
         version = {'version': version__mgraph_ai_service_github_digest}
-        assert self.deploy_fast_api__dev .invoke__function_url('/info/version') == version
+        assert self.deploy_fast_api__main .invoke__function_url('/info/version') == version
 
     # def test_4__delete(self):
-    #     assert self.deploy_fast_api__dev .delete() is True
+    #     assert self.deploy_fast_api__main .delete() is True
