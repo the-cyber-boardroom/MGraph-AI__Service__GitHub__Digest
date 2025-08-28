28f7229ed070aa0752eccb9436ac68c7fbf9a983
Dinis Cruz <dinis.cruz@owasp.org>
2025-08-28 03:32:25 +0100

    increased osbot-fast-api-serverless version number

--- mgraph_ai_service_github_digest/config.py
+++ mgraph_ai_service_github_digest/config.py
@@ -3,4 +3,4 @@ from mgraph_ai_service_github_digest import package_name
 SERVICE_NAME                             = package_name
 FAST_API__TITLE                          = "Github Digest - Service"
 LAMBDA_NAME__SERVICE__GITHUB_DIGEST      = f'service__{SERVICE_NAME}'
-LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS = ['osbot-fast-api-serverless==v1.2.0']
+LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS = ['osbot-fast-api-serverless==v1.15.0']
